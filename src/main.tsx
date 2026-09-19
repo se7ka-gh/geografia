636f0c804c1d0916d91f07e629d992efe219daa1
se7ka <se7ka.bui@gmail.com>
2026-09-19 18:54:15 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom' // <-- ВАЖНО: HashRouter вместо BrowserRouter
+import App from './App.tsx'
+import './index.css' // Убедись, что этот файл существует, иначе удали эту строку
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+)
